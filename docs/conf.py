c4926d93f84d5752094c31bca2f0a124a6e0d313
denisneuf <denisneuf@hotmail.com>
2021-09-19 00:17:06 +0200

    rst

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
