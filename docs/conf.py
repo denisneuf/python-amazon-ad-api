0e7e1910778220edb599d8f0d854f781cc580eb3
denisneuf <denisneuf@hotmail.com>
2021-09-19 00:52:54 +0200

    rst

--- docs/conf.py
+++ docs/conf.py
@@ -12,20 +12,9 @@
 #
 import os
 import sys
-# This will include the necessary source files folders in the PATH to be able to generate the documentation from.
-# folder = '../venv/lib/python3.8/site-packages/ad_api/api/sp/'
-# folder = '../venv/lib/python3.8/site-packages/ad_api/'
 
-# -- Project information -----------------------------------------------------
-
-project = 'Python Amazon Advertising Api v0.0.2'
-copyright = '2021, daniel'
-author = 'daniel'
-
-# The full version, including alpha/beta/rc tags
-release = '2021'
-
-master_doc = 'index'
+submodules:
+  include: all
 
 extensions = [
     'sphinx.ext.autodoc',
