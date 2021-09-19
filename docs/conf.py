4c77d899b22b2d7b4375abae787e50ad02521ffe
denisneuf <denisneuf@hotmail.com>
2021-09-19 20:55:00 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'autodoc-example'
+project = u'Python Amazon Advertising API'
 copyright = u'2021, DAC'
 author = u'DAC'
 
