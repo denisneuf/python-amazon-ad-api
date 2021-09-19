50eb15f2cef61bf381399e583880841e8eafee30
denisneuf <denisneuf@hotmail.com>
2021-09-19 03:05:38 +0200

    rst

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,11 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+folder = '../ad_api/api/'
+
+print(os.path.abspath(folder))
+
+sys.path.insert(0, os.path.abspath(folder))
 
 
 # -- Project information -----------------------------------------------------
