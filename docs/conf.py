c6d1d666a59a59716ca62b72fd5dc185e6d72f9a
denisneuf <denisneuf@hotmail.com>
2021-09-19 21:07:52 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,10 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+html_theme_options = {
+    "collapse_navigation": False
+}
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
