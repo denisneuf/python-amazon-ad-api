f6aa971db228fd48569bc1ebf71881c481afcd02
mkdir700 <mkdir700@gmail.com>
2021-11-08 13:08:46 +0800

    Adding package manager

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ad_api",
+    version="0.0.9",
+    packages=find_packages(),
+    author="denisneuf",
+    author_email="denisneuf@hotmail.com",
+    description="Python Amazon Advertising Api",
+    keywords="Amazon advertising",
+    url="https://python-amazon-ad-api.readthedocs.io/en/latest/",
+    project_urls={
+        "Documentation": "https://python-amazon-ad-api.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/denisneuf/python-amazon-ad-api"
+    }
+)
