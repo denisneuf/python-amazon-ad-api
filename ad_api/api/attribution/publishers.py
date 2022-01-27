9f28efc78481d0cc54a439e9ef12ddb3e3c1adf6
denisneuf <denisneuf@hotmail.com>
2022-01-27 09:52:03 +0800

    Added plural toget_publishers

--- ad_api/api/attribution/publishers.py
+++ ad_api/api/attribution/publishers.py
@@ -3,7 +3,7 @@ from ad_api.base import Client, sp_endpoint, fill_query_params, ApiResponse
 
 class Publishers(Client):
     @sp_endpoint("/attribution/publishers", method="GET")
-    def get_publisher(self, **kwargs) -> ApiResponse:
+    def get_publishers(self, **kwargs) -> ApiResponse:
         """
 
         Args:
