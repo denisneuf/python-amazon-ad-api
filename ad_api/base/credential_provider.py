fe94bc24673df0133cdc2997493058192780f804
guidopancotti <guido.pancotti@gmail.com>
2023-05-13 14:53:46 +0200

    Use FromEnvCredentialProvider only if all required credentials are presente in env

--- ad_api/base/credential_provider.py
+++ ad_api/base/credential_provider.py
@@ -126,8 +126,10 @@ class CredentialProvider():
 
         client_id_env = self._get_env('AD_API_CLIENT_ID'),
         client_secret_env = self._get_env('AD_API_CLIENT_SECRET')
+        refresh_token_env = self._get_env('AD_API_REFRESH_TOKEN')
+        profile_id_env = self._get_env('AD_API_PROFILE_ID')
 
-        if client_id_env is not None and client_secret_env is not None:
+        if client_id_env is not None and client_secret_env is not None and refresh_token_env is not None and profile_id_env is not None:
 
             try:
                 self.credentials = FromEnvCredentialProvider().load_credentials()
