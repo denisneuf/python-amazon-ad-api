3c4d291599086c9cacb6b3d2240ec499565a9bc6
denisneuf <denisneuf@hotmail.com>
2026-03-15 17:04:43 +0800

    clawdbot fix

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,27 @@
-# Configuration file for Sphinx - ENFOQUE RADICAL
-# NO intenta importar el módulo, solo mockea TODO
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import inspect
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
 
 import os
 import sys
 
-# Agregar directorio padre al path (por si acaso)
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+import ad_api
+
+# -- Project information -----------------------------------------------------
 
 # -- Project information -----------------------------------------------------
 project = 'Python Amazon Advertising API'
@@ -15,61 +31,68 @@ release = '0.7.4'
 version = '0.7.4'
 
 # -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
+    "sphinx_rtd_theme", 'button', 'cookieconsent',
+    'sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon',
+    'enum_tools.autoenum',
+
 ]
 
-# MOCKEAR TODO - ENFOQUE RADICAL
-autodoc_mock_imports = [
-    # Dependencias del sistema
-    'requests',
-    'six',
-    'cachetools',
-    'pycryptodome',
-    'pytz',
-    'confuse',
-    'python-dotenv',
-    'pyyaml',
-    
-    # Módulos del paquete - MOCKEAR TODO EL PAQUETE
-    'ad_api',
-    'ad_api.base',
-    'ad_api.api',
-    'ad_api.api.sp',
-    'ad_api.api.sb',
-    'ad_api.api.sd',
-    'ad_api.api.dsp',
-    'ad_api.auth',
-]
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
 
-# Configuración AGRESIVA de autodoc
-autodoc_default_options = {
-    'members': True,
-    'undoc-members': True,
-    'private-members': False,
-    'special-members': '__init__',
-    'inherited-members': True,
-    'show-inheritance': True,
-    'member-order': 'bysource',
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+autodoc_default_options = {"members": True, "undoc-members": True, 'member-order': 'bysource'}
+# html_theme_options = {
+#     "collapse_navigation": False
+# }
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+# html_theme = 'sphinx_rtd_theme'
+# html_theme = "pydata_sphinx_theme"
+html_theme = 'alabaster'
+
+html_theme_options = {
+    'github_button': True,    # optional tweaks
+    'github_banner': True,
+    'github_button': True,
+    'description': '''
+A wrapper to access Amazon's Selling Partner API with an easy-to-use interface.
+    ''',
+    'donate_url': 'https://github.com/sponsors/saleweaver',
+    'github_user': 'saleweaver',
+    'github_repo': 'python-amazon-sp-api',
+    "github_type": "star",  # watch|star|fork
+'fixed_sidebar': True
 }
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_css_files = ['custom.css']
 
-# CRÍTICO: Forzar que funcione incluso con módulos mockeados
-autodoc_inherit_docstrings = True
-autodoc_preserve_defaults = True
-autodoc_typehints = 'none'  # Simplificar
 
-# Napoleon para docstrings estilo Google
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
-napoleon_include_init_with_doc = True
-napoleon_include_private_with_doc = False
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = True
 napoleon_include_special_with_doc = True
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-# -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
